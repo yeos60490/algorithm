7b842ab4a1e3b0f55c058695861b9822ea780ea3
songheeyeon <yeos60490@naver.com>
2021-05-17 04:47:10 +0900

    020517

---
+++ hard/regular_expression_matching.py
@@ -0,0 +1,38 @@
+#작업중
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        print(p)
+        while p[0] != '.' and p[0] != s[0] and p[1] == "*":
+            p = p[2:]
+        
+        #print(p)
+        j = 0
+        for i in range(len(p)):
+            #j += 1
+            print("i: ", i, p[i])
+            print("j: " , j, s[j])
+            if p[i] == s[j]:
+                j += 1
+                #print(1)
+            elif p[i] == ".":
+                j += 1
+                #print(2)
+            elif p[i] == "*":
+                while p[i-1] == s[j]:
+                    if j == len(s)-1:
+                        print("out")
+                        break
+                    j += 1
+                    
+            else:
+                #print(4)
+                return False
+            
+            
+            
+        if j < len(s)-1:
+            return False
+        else:
+            return True
+                
+            
