1b9ba96f6696555928cac33e4a07257221fce35b
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-07-24 00:12:43 +0900

    210724

---
+++ leetcode/medium/validate-binary-search-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: TreeNode, lessThan=float('inf'), greaterThan=float('-inf')) -> bool:
+
+        if not root:
+            return True
+        
+        elif root.val >= lessThan or root.val <= greaterThan:
+            return False
+        
+        else:
+            return self.isValidBST(root.left, min(root.val, lessThan), greaterThan) and self.isValidBST(root.right, lessThan, max(greaterThan, root.val)) 
+        
