74ff28871ba027908fee448b2c11062d77970054
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-07-29 18:37:02 +0900

    210729 주석 추가

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -6,19 +6,20 @@ def solution(n, computers):
     
     def dfs(node, network):
 
-        for i in range(len(network)):
-            if network[i] == 1:
-                if visited[i] == 1:
+        for i in range(len(network)): #node 기준 다른 노드에 대해서
+            if network[i] == 1: #인접 노드인 경우
+                if visited[i] == 1: #이미 방문한 경우
                     continue
-                else:
+                    
+                else: #방문 안한 경우
                     visited[i] = 1
-                    dfs(i, computers[i])
+                    dfs(i, computers[i]) #방문 안한 노드에 다시 dfs
 
         return True
                 
     
     while not all(visited):
         place = visited.index(0)
-        answer += dfs(place, computers[place])
+        answer += dfs(place, computers[place]) 
     
     return answer
