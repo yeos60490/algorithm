22547d373153cf6e151b1b3768415e28203c7edb
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-08-04 20:46:51 +0900

    210804 - collections.Counter

--- programmers/완주하지못한선수.py
+++ programmers/완주하지못한선수.py
@@ -1,6 +1,8 @@
 ## del, remove 썼을 경우 효율성에서 통과하지 않았다
+## collections 사용하면 빠르다
 
 def solution(participant, completion):
+    '''
     participant.sort()
     completion.sort()
     
@@ -9,3 +11,10 @@ def solution(participant, completion):
             return participant[i]
     
     return participant[-1]
+    '''
+    
+    import collections
+    
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    
+    return list(answer.keys())[0]
