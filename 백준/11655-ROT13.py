c4f936ab803f69b03e4779201b11b0024268c450
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-08-06 20:24:55 +0900

    Update 11655-ROT13.py

--- 백준/11655-ROT13.py
+++ 백준/11655-ROT13.py
@@ -1,15 +1,16 @@
-def solution(text):
-	answer = ''
-	for i in text:
-		if (i >= 'A' and i <= 'M') or (i >= 'a' and i <= 'm'):
-			answer += chr(ord(i)+13)
-		elif (i >'M' and i <= 'Z') or (i > 'm' and i <= 'z'):
-			answer += chr(ord(i)-13)
-		else:
-			answer += i
 
-	return answer
+def solution(text):
+    answer = ''
+    for i in text:
+        if (i >= 'A' and i <= 'M') or (i >= 'a' and i <= 'm'):
+            answer += chr(ord(i)+13)
+        elif (i >'M' and i <= 'Z') or (i > 'm' and i <= 'z'):
+            answer += chr(ord(i)-13)
+        else:
+            answer += i
+
+    return answer
 
 
 if __name__ == '__main__':
-	print(solution(input()))
+    print(solution(input()))
