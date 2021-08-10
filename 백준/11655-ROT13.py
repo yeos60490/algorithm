37a104e177f583ecd8403376428801085c7bd45b
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-08-10 18:26:20 +0900

    210810 수정

--- 백준/11655-ROT13.py
+++ 백준/11655-ROT13.py
@@ -1,16 +1,15 @@
-
 def solution(text):
-	answer = ''
-	for i in text:
-		if i.upper() >= 'A' and i.upper() <= 'M':
-			answer += chr(ord(i)+13)
-		elif i.upper() >'M' and i.upper() <= 'Z':
-			answer += chr(ord(i)-13)
-		else:
-			answer += i
+    answer = ''
+    for i in text:
+        if i.upper() >= 'A' and i.upper() <= 'M':
+            answer += chr(ord(i)+13)
+        elif i.upper() >'M' and i.upper() <= 'Z':
+            answer += chr(ord(i)-13)
+        else:
+            answer += i
 
-	return answer
+    return answer
 
 
 if __name__ == '__main__':
-	print(solution(input()))
+    print(solution(input()))
