7fcd46120c0b263fd2a19af96839aa4e641fc668
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-08-17 22:10:48 +0900

    Create 14405-피카츄.py

---
+++ 백준/14405-피카츄.py
@@ -0,0 +1,7 @@
+def solution(text):
+    text = text.replace("pi", "1").replace("ka", "1").replace("chu", "1").replace("1", "")
+
+    return "NO" if text else "YES"
+
+if __name__ == '__main__':
+    print(solution(input()))
