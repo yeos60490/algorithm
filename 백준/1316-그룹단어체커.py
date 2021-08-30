2461251edd10be4b50f85c16cf5a398a609830d5
SongHee <54983764+yeos60490@users.noreply.github.com>
2021-08-31 00:48:07 +0900

    Update 1316-그룹단어체커.py

--- 백준/1316-그룹단어체커.py
+++ 백준/1316-그룹단어체커.py
@@ -1,1 +1,16 @@
-## https://www.acmicpc.net/problem/1316
+def solution(text):
+    arr = [text[0]]
+
+    for i in text[1:]:
+        if i == arr[-1]: pass
+        elif i not in arr: arr.append(i)
+        elif i in arr: return False
+
+    return True
+
+if __name__ == '__main__':
+    answer = 0
+    for i in range(int(input())):
+        answer += solution(input())
+
+    print(answer)
